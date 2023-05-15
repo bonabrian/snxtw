7fb84b2c489ad90a018327417434ca8185228a85
bonabrian <bonabriansiagian@gmail.com>
2023-05-15 16:27:55 +0700

    feat: add ErrorPage

---
+++ src/app/error.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useEffect } from 'react'
+
+interface ErrorPageProps {
+  error: Error
+  reset: () => void
+}
+
+const ErrorPage = ({ error, reset }: ErrorPageProps) => {
+  useEffect(() => {
+    // log the error to the error reporting service
+    console.log(error)
+  }, [error])
+
+  return (
+    <div className="flex items-center justify-center">
+      <p>Oh no, something went wrong... maybe refresh?</p>
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default ErrorPage
