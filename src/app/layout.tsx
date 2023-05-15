558c93221812168d6cde3cc66d547e707cbdd0ba
bonabrian <bonabriansiagian@gmail.com>
2023-05-15 17:45:55 +0700

    feat: implement theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 
 import { Poppins } from 'next/font/google'
 
+import Providers from '@/components/providers'
 const fontSans = Poppins({
   subsets: ['latin'],
   variable: '--font-sans',
@@ -21,7 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={fontSans.variable}>
       <body className="antialiased text-black bg-slate-100 dark:bg-gray-900 dark:text-slate-100">
-        {children}
+        <Providers>
+          <main
+            className="flex flex-col mx-auto max-w-5xl justify-center py-10 px-4"
+            role="main"
+          >
+            <div className="min-h-screen">{children}</div>
+          </main>
+        </Providers>
       </body>
     </html>
   )
