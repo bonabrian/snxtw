c21d36cb25c02be1a6495578affc5b74be7a70a9
bonabrian <bonabriansiagian@gmail.com>
2023-05-17 00:31:56 +0700

    feat: add jest configuration

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+const jestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // If using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  // Absolute imports and Module Path Aliases
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/public/$1',
+  },
+  collectCoverageFrom: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    '!./src/**/_*.{js,jsx,ts,tsx}',
+    '!./src/**/*.stories.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(jestConfig)
