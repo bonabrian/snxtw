61d55c7f13c3c996ba633719d144f5a45e3f4fd6
bonabrian <bonabriansiagian@gmail.com>
2023-06-19 19:26:47 +0700

    refactor: move theme switch to ui components

---
+++ src/components/ui/theme-switch/index.tsx
@@ -0,0 +1,3 @@
+import ThemeSwitch from './theme-switch'
+
+export default ThemeSwitch
---
+++ src/components/ui/theme-switch/theme-switch.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react'
+
+import ThemeSwitch from './theme-switch'
+
+jest.mock('../../../hooks/use-theme', () => {
+  return {
+    useTheme: () => ({
+      theme: 'light',
+      mounted: true,
+      setTheme: jest.fn(),
+    }),
+  }
+})
+
+describe('ThemeSwitch', () => {
+  it('should renders without crashing', () => {
+    expect(() => {
+      render(<ThemeSwitch />)
+    }).not.toThrow()
+  })
+})
