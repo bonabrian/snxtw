3da71793b6216fcb51a1dfc50fcabd5cbebc31bf
bonabrian <bonabriansiagian@gmail.com>
2023-06-22 11:58:09 +0700

    refactor: replace react-icons with own svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,69 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
+import {
+  Eslint,
+  Jest,
+  NextJs,
+  TailwindCss,
+  TypeScript,
+  Vercel,
+} from '@/components/ui/icons'
+import cn from '@/lib/cn'
+
 export default function Home() {
-  const icons = [
+  const stack = [
     {
-      path: '/nextjs.svg',
-      alt: 'Next.js Logo',
-      className: 'dark:invert',
+      icon: <NextJs className={cn('w-6 h-6 fill-black', 'dark:fill-white')} />,
       title: 'Next.js',
     },
     {
-      path: '/tailwind.svg',
-      alt: 'Tailwind Logo',
-      className: '',
+      icon: <TailwindCss className={cn('w-6 h-6 fill-[#06B6D4]')} />,
       title: 'Tailwind CSS',
     },
     {
-      path: '/typescript.svg',
-      alt: 'Typescript Logo',
-      className: '',
-      title: 'Typescript',
+      icon: <TypeScript className={cn('w-6 h-6 fill-[#3178C6]')} />,
+      title: 'TypeScript',
+    },
+    {
+      icon: <Eslint className={cn('w-6 h-6 fill-[#4B32C3]')} />,
+      title: 'Eslint',
+    },
+    {
+      icon: <Jest className={cn('w-6 h-6 fill-[#C21325]')} />,
+      title: 'Jest',
     },
   ]
 
   return (
-    <div className="flex flex-col justify-center min-h-[70vh]">
-      <div className="flex flex-col items-center text-center">
-        <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          className="dark:invert mb-4"
-          width={54}
-          height={24}
-          priority
-        />
-        <h1 className="text-2xl lg:text-3xl font-bold mb-4">
+    <div className={cn('flex flex-col justify-center min-h-[70vh]')}>
+      <div className={cn('flex flex-col items-center text-center')}>
+        <div className={cn('text-current mb-4')}>
+          <Vercel className={cn('w-12 h-12')} />
+        </div>
+        <h1 className={cn('text-2xl font-bold mb-4', 'lg:text-3xl')}>
           Next.js (App Router) + Tailwind CSS + Typescript Starter Template
         </h1>
-        <p className="mb-4">
+        <p className={cn('mb-4')}>
           Next.js bootstrapped by Tailwind CSS and Typescript setup with useful
           development features.
         </p>
-        <div className="flex items-center justify-center mb-8">
+        <div className={cn('flex items-center justify-center mb-8')}>
           <Link
             href="https://github.com/bonabrian/nextjs-starter-tailwind"
             target="_blank"
-            className="border-b border-dotted border-gray-600 dark:border-slate-50 hover:text-primary-500 hover:border-primary-500 dark:hover:border-primary-500"
+            className={cn(
+              'border-b border-dotted border-gray-600 hover:text-primary-500 hover:border-primary-500',
+              'dark:border-slate-50 dark:hover:border-primary-500',
+            )}
           >
             See the repository
           </Link>
         </div>
-        <div className="flex gap-x-4">
-          {icons.map(({ path, alt, className, title }) => (
-            <Image
-              key={path}
-              src={path}
-              alt={alt}
-              className={className}
-              title={title}
-              width={24}
-              height={24}
-            />
+        <div className={cn('flex gap-4')}>
+          {stack.map(({ icon, title }) => (
+            <div key={title} className={cn('flex items-center')}>
+              {icon}
+            </div>
           ))}
         </div>
       </div>
--- src/components/ui/theme-switch/theme-switch.tsx
+++ src/components/ui/theme-switch/theme-switch.tsx
@@ -1,21 +1,29 @@
 'use client'
 
-import { RiMoonFill, RiSunFill } from 'react-icons/ri'
-
 import { useTheme } from '@/hooks'
+import cn from '@/lib/cn'
+
+import { Moon, Sun } from '../icons'
 
 const ThemeSwitch = () => {
   const { theme, mounted, setTheme } = useTheme()
   const isDark = theme === 'dark'
 
   return (
-    <div className="flex items-center w-12 h-6 ml-4">
+    <div className={cn('flex items-center w-12 h-6 ml-4')}>
       {mounted && (
         <button
-          className="flex items-center text-center bg-slate-100 hover:bg-slate-200 dark:bg-gray-800 dark:hover:bg-gray-700 p-2 rounded-full"
+          className={cn(
+            'flex items-center text-center bg-slate-100 hover:bg-slate-200 p-2 rounded-full',
+            'dark:bg-gray-800 dark:hover:bg-gray-700',
+          )}
           onClick={() => setTheme(isDark ? 'light' : 'dark')}
         >
-          {isDark ? <RiSunFill size={24} /> : <RiMoonFill size={24} />}
+          {isDark ? (
+            <Sun className={cn('w-5 h-5')} />
+          ) : (
+            <Moon className={cn('w-5 h-5')} />
+          )}
         </button>
       )}
     </div>
