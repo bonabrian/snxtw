82a8123cd37e7e2658d916f4e47cc924e63d14f2
bonabrian <bonabriansiagian@gmail.com>
2023-08-24 16:43:14 +0700

    feat: init components page

---
+++ src/app/components/page.tsx
@@ -0,0 +1,13 @@
+import cn from '@/lib/cn'
+
+const ComponentsPage = () => {
+  return (
+    <div className={cn('flex flex-col justify-center min-h-screen')}>
+      <div className={cn('flex flex-col items-center text-center')}>
+        <h1>Components Page</h1>
+      </div>
+    </div>
+  )
+}
+
+export default ComponentsPage
