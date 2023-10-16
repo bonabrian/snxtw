e4b6e6e544de09f00c6488964bf886ec6bbabad4
bonabrian <bonabriansiagian@gmail.com>
2023-10-16 23:26:17 +0700

    feat: add @t3-oss/env-nextjs and zod

---
+++ src/data/env/env.mjs
@@ -0,0 +1,90 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+/**
+ * Transform an empty string to undefined
+ */
+const emptyStringToUndefined = z.literal('').transform(() => undefined)
+
+/**
+ * An optional string type that is at least one character long, or transformed
+ * to undefined
+ */
+const optionalString = z
+  .string()
+  .trim()
+  .min(1)
+  .optional()
+  .or(emptyStringToUndefined)
+
+export const env = createEnv({
+  /**
+   * Environment variables available on the client (and server).
+
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   *
+   * Specify your client-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars. To expose them to the client, prefix them with
+   * `NEXT_PUBLIC_`.
+   */
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().url().optional(),
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: optionalString,
+  },
+
+  /**
+   * Server-side Environment variables, not available on the client. Will throw
+   * if you access these variables on the client.
+   *
+   * Specify your server-side environment variables schema here. This way you can ensure the app
+   * isn't built with invalid env vars.
+   */
+  server: {
+    /**
+     * Add your server env here.
+     * For example:
+     *
+     * JWT_SECRET=V3RYS3CR3T
+     * DATABASE_URL=mysql://root:root@localhost:3306/example_db
+     */
+  },
+
+  /**
+   * Shared between server and client
+   */
+  shared: {
+    NODE_ENV: z
+      .enum(['development', 'production', 'test'])
+      .default('development'),
+  },
+
+  /**
+   * Due to how Next.js (>= 13.4.4) bundles environment variables on the Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * Means you can't destruct `process.env` as a regular object during the Next.js edge
+   * runtime (e.g. with middleware) or client-side, so we need to destruct it manually.
+   *
+   * 💡 You'll get type errors if not all variables from `client` are included here.
+   */
+  runtimeEnv: {
+    /** Client */
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+
+    /** Server */
+    /**
+     * Add your server env here.
+     * For example:
+     *
+     * JWT_SECRET=V3RYS3CR3T
+     * DATABASE_URL=mysql://root:root@localhost:3306/example_db
+     */
+  },
+
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+   * This is especially useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+})
